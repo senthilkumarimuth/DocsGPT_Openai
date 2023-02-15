194b4a61280eed81c764dd1bc6061790d2555043
tvs_senthil <senthilkumarimuthu@gmail.com>
2023-02-15 18:48:29 +0530

    prompt udpated

--- application/app.py
+++ application/app.py
@@ -1,54 +1,50 @@
-import pickle
-import dotenv
 from flask import Flask, request, render_template
 import pickle
 import dotenv
 import os
 from utils import answer_query_with_context
 
-# Redirect PosixPath to WindowsPath on Windows
-import platform
-if platform.system() == "Windows":
-    import pathlib
-    temp = pathlib.PosixPath
-    pathlib.PosixPath = pathlib.WindowsPath
-
 # loading the .env file
 dotenv.load_dotenv()
 
+# load prompt template
 with open("combine_prompt.txt", "r") as f:
     template = f.read()
 
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/api/answer", methods=["POST"])
 def api_answer():
     data = request.get_json()
     question = data["question"]
-    # check if the vectorstore is set\
+
+    # check if the vector store is set
     if data['active_docs'] !='Choose documentation':
-        vectorstore = "vectorstores/" + data["active_docs"]
-
+        vector_store = "vectorstores/" + data["active_docs"]
     else:
-        vectorstore = ""
-    #print(vectorstore)
-    with open(os.path.join(vectorstore,"df.pkl"), "rb") as f:
+        vector_store = ""
+    # load pkl from vector store
+    with open(os.path.join(vector_store,"df.pkl"), "rb") as f:
         df = pickle.load(f)
-    with open(os.path.join(vectorstore,"document_embeddings.pkl"), "rb") as f:
+    with open(os.path.join(vector_store,"document_embeddings.pkl"), "rb") as f:
         document_embeddings = pickle.load(f)
-    # loading the index and the store and the prompt template
 
+    # loading the index and the store and the prompt template
     answer = answer_query_with_context(question, df, document_embeddings, template, show_prompt=True)
-    result = {}
-    result['answer'] = answer
+    result = {'answer': answer}
     # some formatting for the frontend
+    temp = result['answer'].split('SOURCES:')
     result['answer'] = result['answer'].replace("\\n", "<br>")
-    # result['answer'] = result['answer'].replace("SOURCES:", "")
+    result['answer'] = temp[0]
+    print('SOURCE: ', temp[1])
+    #result['answer'] = result['answer'].replace("SOURCES:", "")
     return result
 
 
