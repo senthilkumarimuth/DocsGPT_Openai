de821e4c37635c178ae40594d0ef83cb33c260a4
tvs_senthil <senthilkumarimuthu@gmail.com>
2023-02-15 15:52:43 +0530

    utils file added

---
+++ application/utils.py
@@ -0,0 +1,109 @@
+import openai
+import numpy as np
+import tiktoken
+import pandas as pd
+
+COMPLETIONS_MODEL = "text-davinci-003"   ##Todo: Babbage/Curie models may be more suitable. Need to test this
+EMBEDDING_MODEL = "text-embedding-ada-002"
+
+
+MAX_SECTION_LEN = 500
+SEPARATOR = "\n* "
+ENCODING = "gpt2"  # encoding for text-davinci-003
+
+encoding = tiktoken.get_encoding(ENCODING)
+separator_len = len(encoding.encode(SEPARATOR))
+
+f"Context separator contains {separator_len} tokens"
+
+COMPLETIONS_API_PARAMS = {
+    # We use temperature of 0.0 because it gives the most predictable, factual answer.
+    "temperature": 0.0,
+    "max_tokens": 300,
+    "model": COMPLETIONS_MODEL,
+}
+
+
+
+
+def get_embedding(text: str, model: str = EMBEDDING_MODEL) -> list[float]:
+    result = openai.Embedding.create(
+        model=model,
+        input=text
+    )
+    return result["data"][0]["embedding"]
+
+def vector_similarity(x: list[float], y: list[float]) -> float:
+    """
+    Returns the similarity between two vectors.
+
+    Because OpenAI Embeddings are normalized to length 1, the cosine similarity is the same as the dot product.
+    """
+    return np.dot(np.array(x), np.array(y))
+
+def order_document_sections_by_query_similarity(query: str, contexts: dict[(str, str), np.array]) -> list[
+    (float, (str, str))]:
+    """
+    Find the query embedding for the supplied query, and compare it against all of the pre-calculated document embeddings
+    to find the most relevant sections.
+
+    Return the list of document sections, sorted by relevance in descending order.
+    """
+    query_embedding = get_embedding(query)
+
+    document_similarities = sorted([
+        (vector_similarity(query_embedding, doc_embedding), doc_index) for doc_index, doc_embedding in contexts.items()
+    ], reverse=True)
+
+    return document_similarities
+
+def construct_prompt(question: str, context_embeddings: dict, df: pd.DataFrame) -> str:
+    """
+    Fetch relevant
+    """
+    most_relevant_document_sections = order_document_sections_by_query_similarity(question, context_embeddings)
+
+    chosen_sections = []
+    chosen_sections_len = 0
+    chosen_sections_indexes = []
+
+    for _, section_index in most_relevant_document_sections:
+        # Add contexts until we run out of space.
+        document_section = df.loc[section_index]
+
+        chosen_sections_len += document_section.tokens + separator_len
+        if chosen_sections_len > MAX_SECTION_LEN:
+            break
+
+        chosen_sections.append(SEPARATOR + document_section.content.replace("\n", " "))
+        chosen_sections_indexes.append(str(section_index))
+
+    # Useful diagnostic information
+    print(f"Selected {len(chosen_sections)} document sections:")
+    print("\n".join(chosen_sections_indexes))
+
+    header = """You are TVS QA BOT. You are capable of answering questions reqarding TVS Owner Manual. Answer the question as truthfully as possible using the provided context, and if the answer is not contained within the text below, say "I don't know."\n\nContext:\n"""
+
+    return header + "".join(chosen_sections) + "\n\n Q: " + question + "\n A:"
+
+
+def answer_query_with_context(
+        query: str,
+        df: pd.DataFrame,
+        document_embeddings: dict[(str, str), np.array],
+        show_prompt: bool = False) -> str:
+    prompt = construct_prompt(
+            query,
+            document_embeddings,
+            df
+        )
+
+    if show_prompt:
+        print(prompt)
+
+    response = openai.Completion.create(
+        prompt=prompt,
+        **COMPLETIONS_API_PARAMS
+    )
+
+    return response["choices"][0]["text"].strip(" \n")
